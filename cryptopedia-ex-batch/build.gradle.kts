c80b42c94fd36bea1dd51e249c3d29d3440a18ac
joohyun.ha <mylifeforcoding@gmail.com>
2026-01-15 01:45:21 +0900

    fix(batch): add spring-web dependency

--- cryptopedia-ex-batch/build.gradle.kts
+++ cryptopedia-ex-batch/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     implementation(project(":cryptopedia-core"))
 
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
 
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
